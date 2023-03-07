c42475ce507a7637478c6bd314e5cbc56064b655
Raja Babu <rajababudas7563977671@gmail.com>
2023-03-07 19:32:19 +0530

    ELectric Engine Stopped

--- ElectricEngine.java
+++ ElectricEngine.java
@@ -2,4 +2,7 @@ public class ElectricEngine{
 	public void start() {
 		System.out.println("Electric Engine started");
 	}
+	public void stop() {
+		System.out.println("Electric Engine stopped");
+	}
 }
