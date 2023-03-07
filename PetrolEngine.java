a0f8f219d6fdc53f0f60b708198a5bb32b0c816b
Raja Babu <rajababudas7563977671@gmail.com>
2023-03-07 19:29:04 +0530

    Created ElectricEngine.java class

--- PetrolEngine.java
+++ PetrolEngine.java
@@ -2,5 +2,11 @@ public class PetrolEngine{
 	public void start() {
 		System.out.println("Hello!");
 	}
+	public void stop() {
+		System.out.println("Disel has stop");
+	}
+	public void VehicleSound() {
+		System.out.println("Disel has beww beww");
+	}
 		
 }
--- vehicle.java
+++ vehicle.java
@@ -2,8 +2,10 @@ public class vehicle{
 	public static void main(String[] args) {
 		PetrolEngine pE = new PetrolEngine();
 		pE.start();
+		pE.stop();
 		
 		DeselEngine dE = new DeselEngine();
 		dE.start();
+		dE.stop();
 	}
 }
